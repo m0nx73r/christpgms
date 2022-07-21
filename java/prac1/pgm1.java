a587f295d8c187f4377ae85adb028ff97a38eaae
Aditya Kumar <naammkyahai@gmail.com>
2022-07-21 19:29:52 +0530

    Create pgm1.java

---
+++ java/prac1/pgm1.java
@@ -0,0 +1,12 @@
+package pgm1;
+
+public class pgm1 {
+	public static void main(String[] args) {
+		
+		System.out.println("Name : Aditya Kumar");
+		System.out.println("Roll number : 21215008");
+		System.out.println("Class : 3BCA");
+		System.out.println("Address : Marium Nagar");
+		System.out.println("Phone number : 7909058XXX");
+	}
+}
